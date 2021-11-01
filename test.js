43ac98adc9b8e82ff75e0908c433d4be6f4843c1
hbk671104 <hbk671104@gmail.com>
2021-11-01 18:39:10 +0800

    complete test cases

--- test.js
+++ test.js
@@ -2,23 +2,35 @@ const yargs = require('yargs/yargs')
 const { hideBin } = require('yargs/helpers')
 const { argv } = yargs(hideBin(process.argv))
 
-// const { startCrawling, saveProject } = require('./app')
-const { getProjectIDs } = require('./task')
+const { getProjectIDs, getProjectDetail, saveProject } = require('./task')
 
-// if (argv.saveAll) {
-//     startCrawling()
-// }
+if (argv.startAll) {
+    console.log('test crawl all...')
+    startCrawling()
+}
 
-// if (argv.id) {
-//     saveProject(argv.id)
-// }
-
-if (argv.getIDs) {
-    console.log('test get projects...')
+if (argv.id) {
+    console.log(`test get project detail: ${argv.id}...`)
     ;(async () => {
         try {
-            const list = await getProjectIDs()
-            console.log(list.length)
+            const result = await getProjectDetail(argv.id)
+            console.log(result)
+            if (argv.save) {
+                await saveProject(result)
+                console.log(`${argv.id} object saved.`)
+            }
+        } catch (error) {
+            console.error(error)
+        }
+    })()
+}
+
+if (argv.getIDs) {
+    console.log('test get project ids...')
+    ;(async () => {
+        try {
+            const ids = await getProjectIDs()
+            console.log(ids.length)
         } catch (error) {
             console.error(error)
         }
